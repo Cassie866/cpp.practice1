5645243ecedb09763704ebd7639ce119a531551c
Cassie <cassie866@163.com>
2019-10-14 21:36:35 +0800

    Linux结课算法

---
+++ 考试/源.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int n = 0;
+	cin >> n;
+	int a[1000];
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		scanf("%d", &x);
+		a[i] = x;
+	}
+	int k = 0;
+	cin >> k;
+	sort(a,a+n);
+	int len = unique(a, a + n) - a;
+	if (k < len)cout << a[k - 1];
+
+	return 0;
+}
+
+
+
